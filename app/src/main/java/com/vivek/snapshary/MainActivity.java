24fea4e7e4679b4a49725500b5f007eeca028ef3
vksah32 <vksah@colby.edu>
2015-07-22 00:22:01 -0400

    added add friends and create session

--- app/src/main/java/com/vivek/snapshary/MainActivity.java
+++ app/src/main/java/com/vivek/snapshary/MainActivity.java
@@ -1,30 +1,45 @@
 package com.vivek.snapshary;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.text.Layout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private ParseObject mUser;
+    static public ParseObject mUser;
     private EditText mPhone;
-    private  Session mSession;
+    static public  ParseObject mSession;
+    private String mSessionID;
+    private String mAndroidID;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        doesUserExists();
+
+        //only if google account is associated
         setContentView(R.layout.activity_main);
-        mPhone = (EditText) findViewById(R.id.signUpPhone);
 
     }
 
@@ -50,12 +65,61 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void invitePeople(View v){
+    public void createSession(View v){
 
 //       mSession = new Session(mPhone.getText().toString());
-       Intent i = new Intent(MainActivity.this, inviteActivity.class);
-        i.putExtra("Phone",mPhone.getText().toString() );
-       startActivity(i);
+        mSession = new ParseObject("Session");
+        mSession.put("creator", mUser);
+        mSession.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                mSessionID = mSession.getObjectId();
+                Log.i("SESSIONID", mSessionID);
+                Intent i = new Intent(MainActivity.this, SessionCreated.class);
+                i.putExtra("Code",mSessionID);
+                startActivity(i);
+
+            }
+        });
+
+
+
+    }
+
+    public void joinSession(View v){
+        startActivity(new Intent(MainActivity.this, JoinSession.class));
+
+    }
+
+    public String getAndroidID(){
+        return Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    public void doesUserExists(){
+        ParseQuery<ParseObject> userQuery = ParseQuery.getQuery("User");
+        userQuery.whereEqualTo("androidID", getAndroidID());
+        Log.i("ANDROID ID", getAndroidID());
+        userQuery.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> userList, ParseException e) {
+                if (e == null) {
+                    if (userList.size() == 0)
+                        setUser();
+                    else
+                        mUser = userList.get(0);
+
+                } else {
+                    Log.d("USER", "Error: " + e.getMessage());
+                }
+            }
+        });
+
+    }
+
+
+    public void setUser(){
+        mUser = new ParseObject("User");
+        mUser.put("androidID", getAndroidID());
+        mUser.saveInBackground();
 
 
     }
